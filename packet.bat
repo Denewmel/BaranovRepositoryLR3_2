ac8b085989fc33762e7fc349d3d8df315d2668bd
Denewmel <denewmel@gmail.com>
2023-12-24 18:31:02 +0300

    1.0
    Main commit

---
+++ packet.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE
+vcn.exe" baranovProject.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" myConfig
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" build.iss
